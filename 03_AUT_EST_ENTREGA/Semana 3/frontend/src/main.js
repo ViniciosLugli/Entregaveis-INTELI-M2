cad59d6a7c4c0cf4538ea5b251580a52501e6752
ViniciosLugli <vinicioslugli@gmail.com>
2022-05-06 17:46:24 -0300

    Change scrollbar style

--- 03_AUT_EST_ENTREGA/Semana 3/frontend/src/main.js
+++ 03_AUT_EST_ENTREGA/Semana 3/frontend/src/main.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
-    function isScrolledIntoView(elem) {
-        // Source from http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
-        var docViewTop = $(window).scrollTop()
-        var docViewBottom = docViewTop + $(window).height()
+$(document).ready(function () {
+	function isScrolledIntoView(elem) {
+		// Function source from http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+		var docViewTop = $(window).scrollTop()
+		var docViewBottom = docViewTop + $(window).height()
 
-        var elemTop = $(elem).offset().top
-        var elemBottom = elemTop + $(elem).height()
+		var elemTop = $(elem).offset().top
+		var elemBottom = elemTop + $(elem).height()
 
-        return elemBottom <= docViewBottom && elemTop >= docViewTop
-    }
+		return elemBottom <= docViewBottom && elemTop >= docViewTop
+	}
 
-    $(window).scroll(function() {
-        $('.scroll-sides .animated').each(function() {
-            if (isScrolledIntoView(this) === true) {
-                if (this.classList.contains('left')) {
-                    $(this).addClass('fadeInLeft')
-                } else if (this.classList.contains('right')) {
-                    $(this).addClass('fadeInRight')
-                } else {
-                    $(this).addClass('fadeIn')
-                }
-            }
-        })
-    })
+	$(window).scroll(function () {
+		$('.scroll-sides .animated').each(function () {
+			if (isScrolledIntoView(this) === true) {
+				if (this.classList.contains('left')) {
+					$(this).addClass('fadeInLeft')
+				} else if (this.classList.contains('right')) {
+					$(this).addClass('fadeInRight')
+				} else {
+					$(this).addClass('fadeIn')
+				}
+			}
+		})
+	})
 })
